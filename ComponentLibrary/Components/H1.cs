6ebe88a35b4282cf3a8a88871ba257c9c03155e9
rob <robert.hollingworth@vcreate.tv>
2024-01-18 18:16:35 +0000

    component library working correctly

--- ComponentLibrary/Components/H1.cs
+++ ComponentLibrary/Components/H1.cs
@@ -1,14 +1,36 @@
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
 using Avalonia.Styling;
 
-namespace ComponentLibrary;
+namespace ComponentLibrary {
+    public class H1() : Header("FontSize.H1");
 
-public class H1 : TextBlock, IStyleable {
-    public H1() {
-        FontSize = 32; // equivalent to <H1> in HTML
-        FontWeight = FontWeight.Bold;
+    public class H2() : Header("FontSize.H2");
+
+    public class H3() : Header("FontSize.H3");
+
+    public class H4() : Header("FontSize.H4");
+
+    public class H5() : Header("FontSize.H5");
+
+    public class Header : TextBlock {
+        private readonly string _fontSizeKey;
+
+        public Header(string fontSizeKey) {
+            _fontSizeKey = fontSizeKey;
+            FontWeight = Avalonia.Media.FontWeight.Bold;
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e) {
+            base.OnAttachedToVisualTree(e);
+
+            if(this.TryFindResource(_fontSizeKey, out var value)) {
+                FontSize = (double)value;
+            } else {
+                FontSize = 4;
+            }
+        }
+
+        protected override Type StyleKeyOverride => typeof(TextBlock);
     }
-
-    Type IStyleable.StyleKey => typeof(TextBlock);
 }
